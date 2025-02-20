df61f8792c3ee4f990750590e6b336a7187a7f68
pwojton <piotr.wojton.01@gmail.com>
2025-02-20 18:17:20 +0100

    Create custom TokenObtainPairView

--- backend/authapp/views.py
+++ backend/authapp/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from typing import Any
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.exceptions import AuthenticationFailed
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        try:
+            response: Response = super().post(request, *args, **kwargs)
+            tokens: dict[str, str] = response.data
+
+            access_token: str = tokens['access']
+            refresh_token: str = tokens['refresh']
+
+            res: Response = Response()
+
+            res.data = {'Successful login': True}
+            res.set_cookie(
+                key='access_token',
+                value=access_token,
+                httponly=True,
+                secure=True,
+                samesite='None',
+                path='/'
+            )
+            res.set_cookie(
+                key='refresh_token',
+                value=refresh_token,
+                httponly=True,
+                secure=True,
+                samesite='None',
+                path='/'
+            )
+
+            return res
+        except AuthenticationFailed:
+            return Response(data={'Successful login': False})
