1a40adbfcadf44005546aa4808680da6d148991a
pwojton <piotr.wojton.01@gmail.com>
2025-02-21 18:06:39 +0100

    Make documentation of custom TokenObtainPairView

--- backend/authapp/views.py
+++ backend/authapp/views.py
@@ -7,7 +7,30 @@ from rest_framework.exceptions import AuthenticationFailed
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
+    """
+    Custom view for obtaining JWT authentication tokens.
+    This view extends `TokenObtainPairView` to handle JWT authentication,
+    returning access and refresh tokens as HttpOnly cookies upon successful login.
+
+    Methods:
+        post(request, *args, **kwargs):
+            Authenticates the user and returns tokens as HttpOnly cookies.
+    Attributes:
+        Inherits all attributes from `TokenObtainPairView`.
+    Raises:
+        AuthenticationFailed: If authentication fails due to invalid credentials.
+    """
+
     def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        """
+        Handles user authentication and sets JWT tokens as HttpOnly cookies.
+
+        :param request: The HTTP request containing user credentials.
+        :param args: Additional positional arguments passed to the parent class.
+        :param kwargs: Additional keyword arguments passed to the parent class.
+        :return: A Response object indicating login success and setting cookies.
+        :raises AuthenticationFailed: If authentication credentials are invalid.
+        """
         try:
             response: Response = super().post(request, *args, **kwargs)
             tokens: dict[str, str] = response.data
